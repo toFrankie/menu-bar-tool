4eb9dc1457cbb74897a96b397fd7c0b594c75817
Frankie <1426203851@qq.com>
2023-12-24 16:04:33 +0800

    feat: using electron-vite

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ```shell
 # 跳过下载 Electron 二进制文件
-$ ELECTRON_SKIP_BINARY_DOWNLOAD=1 yarn add menubar
+$ ELECTRON_SKIP_BINARY_DOWNLOAD=1 pnpm add menubar
 ```
--- src/app.tsx
+++
@@ -1,6 +0,0 @@
-import Index from './pages/index/index'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-
-const root = createRoot(document.body)
-root.render(<Index />)
